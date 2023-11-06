12acd09a64a7e96eb705c04983146a8d0868f5c9
kosaboten <saboten.system@gmail.com>
2023-11-06 15:51:13 +0900

    エラー表示

---
+++ database/migrations/2023_11_06_152711_create_portfolios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('image');
+            $table->string('affiliation');
+            $table->text('self_introduction');
+            $table->text('work_experience');
+            $table->string('region');
+            $table->string('twitter');
+            $table->string('facebook');
+            $table->string('instagram');
+            $table->integer('public_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('portfolios');
+    }
+};
